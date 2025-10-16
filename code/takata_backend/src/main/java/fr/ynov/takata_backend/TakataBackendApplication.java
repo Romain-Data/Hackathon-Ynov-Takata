96f4653807da0e62565a2468adb49fe113fbbee2
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-10-16 11:44:04 +0200

    Initialisation back-end spring boot nom takata_backend

---
+++ code/takata_backend/src/main/java/fr/ynov/takata_backend/TakataBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.ynov.takata_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakataBackendApplication.class, args);
+	}
+
+}
