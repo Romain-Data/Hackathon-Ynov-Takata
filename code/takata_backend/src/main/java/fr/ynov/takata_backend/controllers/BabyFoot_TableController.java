39afb41aa31e58ae8d187f1098260d8989b17f53
Bastien ANTRAYGUES <bastien-toulouse@outlook.fr>
2025-10-17 08:37:37 +0200

    Ajout des controllers GameController, UserController, User_GameController, Modification sur BabyFoot_TableController.

--- code/takata_backend/src/main/java/fr/ynov/takata_backend/controllers/BabyFoot_TableController.java
+++ code/takata_backend/src/main/java/fr/ynov/takata_backend/controllers/BabyFoot_TableController.java
@@ -1,9 +1,14 @@
 package fr.ynov.takata_backend.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import fr.ynov.takata_backend.dto.BabyFoot_TableDto;
 import fr.ynov.takata_backend.generic.GenericController;
 import fr.ynov.takata_backend.services.BabyFoot_TableService;
 
+@RestController
+@RequestMapping("api/babyfoot_table")
 public class BabyFoot_TableController extends GenericController<BabyFoot_TableDto, Long, BabyFoot_TableService> {
 
 	public BabyFoot_TableController(BabyFoot_TableService service) {
